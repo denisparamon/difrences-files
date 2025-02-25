f10d6ec1aa52ff7b0eb75cd03446203235efab5f
Hooman <denis_paramon@icloud.com>
2025-02-25 18:12:58 +0500

    fix1.45

--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -5,34 +5,71 @@ declare(strict_types=1);
 namespace Tests;
 
 use JsonException;
-use PHPUnit\Framework\Assert;
 use PHPUnit\Framework\TestCase;
 
 use function Differ\Differ\genDiff;
 
-function testDiff(
-    string $formatter,
-    string $firstFileType,
-    string $secondFileType
-): void {
-    $fixturesPath = __DIR__ . '/fixtures';
-
-    $getExpectedPath = fn(string $formatter) => "{$fixturesPath}/{$formatter}-expected.txt";
-    $getFirstFilePath = fn(string $fileType) => "{$fixturesPath}/file1.{$fileType}";
-    $getSecondFilePath = fn(string $fileType) => "{$fixturesPath}/file2.{$fileType}";
-
-    $diff = genDiff($getFirstFilePath($firstFileType), $getSecondFilePath($secondFileType), $formatter);
-
-    Assert::assertStringEqualsFile($getExpectedPath($formatter), $diff);
-}
-
-function dataProvider(): array
+class DifferTest extends TestCase
 {
-    return [
-        ['stylish', 'json', 'json'],
-        ['stylish', 'yaml', 'json'],
-        ['plain', 'yaml', 'yml'],
-        ['json', 'json', 'json'],
-        ['json', 'yaml', 'yml'],
-    ];
+    private string $fixturesPath = __DIR__ . '/fixtures';
+
+    private function getExpectedPath(string $formatter): string
+    {
+        return "{$this->fixturesPath}/{$formatter}-expected.txt";
+    }
+
+    private function getFirstFilePath(string $fileType): string
+    {
+        return "{$this->fixturesPath}/file1.{$fileType}";
+    }
+
+    private function getSecondFilePath(string $fileType): string
+    {
+        return "{$this->fixturesPath}/file2.{$fileType}";
+    }
+
+    /**
+     * @dataProvider dataProvider
+     * @throws JsonException
+     */
+    public function testDiff(
+        string $formatter,
+        string $firstFileType,
+        string $secondFileType
+    ): void {
+        $diff = genDiff($this->getFirstFilePath($firstFileType), $this->getSecondFilePath($secondFileType), $formatter);
+
+        TestCase::assertStringEqualsFile($this->getExpectedPath($formatter), $diff);
+    }
+
+    public function dataProvider(): array
+    {
+        return [
+            'stylish format, json - json' => [
+                'stylish',
+                'json',
+                'json',
+            ],
+            'stylish format, yaml - json' => [
+                'stylish',
+                'yaml',
+                'json',
+            ],
+            'plain format, yaml - yml' => [
+                'plain',
+                'yaml',
+                'yml',
+            ],
+            'json format, json - json' => [
+                'json',
+                'json',
+                'json',
+            ],
+            'json format, yaml - yml' => [
+                'json',
+                'yaml',
+                'yml',
+            ],
+        ];
+    }
 }
